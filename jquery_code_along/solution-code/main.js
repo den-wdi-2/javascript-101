129e1890cedb4490d54cb28bd9c8b16f071ec051
ZebGirouard <timzebgir@gmail.com>
2016-10-04 10:02:50 -0600

    Adding solution code

--- jquery_code_along/solution-code/main.js
+++ jquery_code_along/solution-code/main.js
@@ -1,21 +1,24 @@
+
 /**
 * Part 1 - Together:
 */
 
 // 1. Remove the highlight class from #box2 (use the .removeClass() method)
-$("#box2").removeClass("highlight");
+$('#box2').removeClass('highlight');
+
 // 2. Select the anchor within #box3. Change the HTML to 'jQuery!'
-$("#box3 g").html("jQuery!");
+$('#box3').find('a').html('jQuery!');
+// or $('#box3 a').html('jQuery');
 
 /**
 * Part 1 - Now you try:
 */
 
 // 1. Add the .highlight class to #box4 (use the .addClass() method)
-$("#box4").addClass("highlight");
+$('#box4').addClass('highlight');
 // 2. Make the text color of the strong tag within #box2 green (use the .css() method)
-$("#box2 strong").css("color", "green");
-
+$('#box2').find('strong').css('color', 'green');
+// or $('#box2 strong').css('color', 'green');
 /**-----------------------------------------------------------------------------------------------*/
 
 
@@ -26,40 +29,46 @@ $("#box2 strong").css("color", "green");
 */
 
 // 1. When the anchor within #box3 is clicked
-	// a) Slide toggle #box4
-	// b) Fade out #box1 slowly
-$("#box3 a").on("click", function() {
-	$("#box4").slideToggle();
-	$("#box1").fadeOut("slow");
+  // a) Slide toggle #box4
+  // b) Fade out #box1 slowly
+$('#box3 a').on('click', function () {
+  $('#box4').slideToggle();
+  $('#box1').fadeOut();
 });
+
 /**
 *
 * Part 2 - Now you try:
 *
 */
 // 1. When the h2 is clicked on
-	// a) Slide up the h2
-	// b) Add the snazzy class to the h1
-$("h2").on("click", function() {
-	$("h2").slideUp();
-	$("h1").addClass("snazzy");
+  // a) Slide up the h2
+  // b) Add the snazzy class to the h1
+$('h2').on('click', function(){
+  $('h2').slideUp();
+  $('h1').addClass('snazzy');
 });
-// 2. When #box1 is clicked on
-//   a) Add the highlight class to h3
-$("#box1").on("click", function() {
-	$("h3").addClass("highlight");
-});
-// Part 3 - Together:
 
+// 2. When #box1 is clicked on
+// a) Add the highlight class to h3
+$('#box1').on('click', function(){
+  $('h3').addClass('highlight');
+});
+
+// Part 3 - Together:
 $(document).ready(function(){
   // 1. Create a variable and name it 'school'. Set the value of the variable equal to the prompt 'Which school you at right now? :)'
-  var school = prompt("What school you at now?");
-  // 2. Create a conditional statement that says if the value entered in to the prompt is equal to 'General Assembly', change the p tag in the footer to say 'Thanks for stopping by General Assembly!' If the user enters 'GA' change the p tag in the footer to say 'Thanks for stopping by GA!' If the user enters in anything else change the p tag in the footer to say 'Still the footer!'
-  if (school === "GA" || school === "General Assembly") {
-  	$("footer p").html("Thanks for stopping by " + school + "!");
-  }
-  else {
-  	$("footer p").html("Still the footer!");
-  }
+  var school = prompt('Which school you at right now? :)');
 
+  // 2. Create a conditional statement that says if the value entered in to the prompt is equal to 'General Assembly', change the p tag in the footer to say 'Thanks for stopping by General Assembly!' If the user enters 'GA' change the p tag in the footer to say 'Thanks for stopping by GA!' If the user enters in anything else change the p tag in the footer to say 'Still the footer!'
+  if (school === 'General Assembly') {
+      $('footer p').html('Thanks for stopping by General Assembly!');
+
+  } else if (school === 'GA') {
+      $('footer p').html('Thanks for stopping by GA!');
+
+  } else {
+      $('footer p').html('Still the footer!');
+  }
 });
+
